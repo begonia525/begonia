624d7c7b9c43373b58983464a67d54a39153610c
begonia525 <61453631+begonia525@users.noreply.github.com>
2020-04-28 16:50:21 +0800

    2020.04.28静态库和动态库的使用

---
+++ Lib0428/Lib0428/w32.cpp
@@ -0,0 +1,9 @@
+#include"stdafx.h"
+#include"w32.h"
+char* pchar()
+{
+	return "´Ó¾²Ì¬¿âÈ¡µÃ×Ö·û´®";
+}
+int F1::GetA() {
+	return a + 1;
+}
---
+++ Lib0428/Lib0428/w32.h
@@ -0,0 +1,10 @@
+#pragma once
+char*pchar();
+class F1 {
+	int a;
+	F1() { a = 100; }
+	int GetA();
+
+
+
+};
