f268fdb33cbbb5166a84dd9ba3e772fdf2d9844b
begonia525 <61453631+begonia525@users.noreply.github.com>
2020-12-11 19:34:08 +0800

    Signed-off-by: begonia525 <61453631+begonia525@users.noreply.github.com>

---
+++ 客户信息反馈系统/BBL/ClientBll.cs
@@ -0,0 +1,96 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+
+
+    public enum LoginState
+    {
+        /// <summary>
+        /// 用户名不存在
+        /// </summary>
+        NoName,
+        /// <summary>
+        /// 密码错误
+        /// </summary>
+        PwdErr,
+        /// <summary>
+        /// 登录成功
+        /// </summary>
+        OK
+
+    }
+
+
+
+
+
+
+    public class ClientBll
+    {
+         ClientDal dal = new ClientDal();
+        public LoginState CheckLogin(string phone, string pwd)//登录判断
+        {
+           ClientDal dal = new ClientDal();
+           Client client = dal.SelectClientInfo(phone);
+            if (client == null)
+                return LoginState.NoName;
+            else
+            {
+                if (client.Pwd != pwd)
+                    return LoginState.PwdErr;
+                else
+                {
+                    return LoginState.OK;
+                }
+            }
+        }
+        
+        public Client SelectClient(string phone)//根据手机号查询该用户的所有信息
+        {
+            return dal.SelectClientInfo(phone);
+        }
+        public Client SelectClient2(int id)//根据id查询该用户的所有信息
+        {
+            return dal.SelectClientInfo2(id);
+        }
+        public bool AddClient(Client client)//新用户注册
+        {
+            return dal.AddClient(client) > 0;
+        }
+        public bool SelectPhoneCount(string phone)//判断该手机号是否存在
+        {
+            return dal.SelectPhoneCount(phone) > 0;
+        }
+        public bool Update(Client client,string phone)//用户修改信息
+        {
+            return dal.Update(client,phone) > 0;
+        }
+
+        public bool adminUpdate(Client client, int id)//管理员修改用户信息
+        {
+            return dal.adminUpdate(client, id) > 0;
+        }
+
+        public bool AdminDelete(int id)//管理员根据ID删除
+        {
+            return dal.AdminDelete(id) > 0;
+        }
+
+        public bool UpdatePwd(string newPwd, string phone)//用户修改密码
+        {
+            return dal.UpdatePwd(newPwd, phone) > 0;
+        }
+
+        public List<Client> GetAll()//根据评价读取数据库中的数据
+        {
+            return dal.GetAll().Count > 0 ? dal.GetAll() : null;
+        }
+    }
+}
---
+++ 客户信息反馈系统/BBL/ReplyBll.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Model;
+
+namespace BLL
+{
+  public  class ReplyBll
+    {
+        ReplyDal replyDal = new ReplyDal();
+        public bool AddReply(Reply reply )//发布反馈
+        {
+            return replyDal.AddFeedBack(reply) > 0;
+        }
+        public bool AddReply(int id ,string reply)
+        {
+            return replyDal.AddReply(id, reply)>0;
+        }
+        public List<Reply> GetAll(string phone)//根据手机号读取数据库中的数据
+        {
+            return replyDal.GetAll(phone).Count > 0 ? replyDal.GetAll(phone) : null;
+        }
+
+        public List<Reply> GetAll2()//读取数据库中的所有数据
+        {
+            return replyDal.GetAll2().Count > 0 ? replyDal.GetAll2() : null;
+        }
+
+        public bool SelectEvaluateCount(string evaluate)//根据手机号查询返回行数
+        {
+            return replyDal.SelectEvaluateCount(evaluate) > 0;
+        }
+        public List<Reply> GetEvaluate(string evaluate)//读取数据库中的数据
+        {
+           
+                return replyDal.GetEvaluate(evaluate).Count > 0 ? replyDal.GetEvaluate(evaluate) : null;
+           
+        }
+        public bool SelectPhoneCount(string phone)//根据手机号查询返回行数
+        {
+            return replyDal.SelectPhoneCount(phone) > 0;
+        }
+        public bool Delete(int id)//根据ID删除
+        {
+            return replyDal.Delete(id) > 0;
+        }
+        public bool Delete2(string phone)//根据手机号删除
+        {
+            return replyDal.Delete2(phone) > 0;
+        }
+
+        public Reply Select(int id)//根据ID查找
+        {
+            return replyDal.Select(id) ;
+        }
+
+        public bool clientUpdate(string name, string phone)//用户修改信息
+        {
+            return replyDal.clientUpdate(name, phone) > 0;
+        }
+
+        public bool adminUpdate(Client client, string phone2)//管理员修改信息
+        {
+            return replyDal.adminUpdate(client, phone2) > 0;
+        }
+    }
+}
